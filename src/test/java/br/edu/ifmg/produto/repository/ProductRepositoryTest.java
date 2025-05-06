691edf23439edf573f99e67767f6ccfc6fedf38d
LuizEduardo150 <luizedluimf10@gmail.com>
2025-05-06 15:28:16 -0300

    aula 10 - Camada de servico

--- src/test/java/br/edu/ifmg/produto/repository/ProductRepositoryTest.java
+++ src/test/java/br/edu/ifmg/produto/repository/ProductRepositoryTest.java
@@ -2,7 +2,9 @@ package br.edu.ifmg.produto.repository;
 
 import br.edu.ifmg.produto.dtos.ProductDTO;
 import br.edu.ifmg.produto.entities.Product;
+import br.edu.ifmg.produto.util.Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +18,40 @@ public class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
+    private Long existingId;
+
+    @BeforeEach
+    void setUo(){
+        existingId = 1L;
+    }
+
     @Test
     @DisplayName(value = "Verificando se o objeto existe depois de ser deletado.")
     public void deleteShouldDeleteObjectWhenIdExists(){
-        productRepository.deleteById(1L);
-        Optional<Product> obj = productRepository.findById(1L);
+        productRepository.deleteById(existingId);
+        Optional<Product> obj = productRepository.findById(existingId);
+
         Assertions.assertFalse(obj.isPresent());
     }
+
+    @Test
+    @DisplayName(value = "Verificando se auto incremento da chave primária.")
+    public void insertShouldPersistWithOutoIncrementIdWhenIdZero(){
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        Product p = productRepository.save(product);
+        Optional<Product> obj = productRepository.findById(p.getId());
+        Assertions.assertTrue(obj.isPresent());
+        Assertions.assertNotEquals(0, obj.get().getId());
+        Assertions.assertEquals(26, obj.get().getId());
+
+
+
+
+
+
+    }
+
     
 }
